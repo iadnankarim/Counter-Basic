7d31a73ff97ee6114c37982cc8892b5138a00174
iadnankarim <adnankarim725@gmail.comm>
2023-04-25 10:40:10 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var currnet=document.querySelector('.current');
+var next=document.querySelector('.next');
+
+function startCounter(){
+    var interval=setInterval(animate , 1000);
+}
+
+function animate(){
+    next.classList.add('animate');
+    setTimeout(function(){
+        next.classList.remove('animate');
+    },500);
+}
